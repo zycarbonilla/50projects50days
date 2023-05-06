747874b7989b74156a429afff073b80f8a40a121
Zybren Carbonilla <zycarbonilla@gmail.com>
2023-05-06 22:01:41 +0800

    drag-n-drop

---
+++ Day21/script.js
@@ -0,0 +1,12 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+}
